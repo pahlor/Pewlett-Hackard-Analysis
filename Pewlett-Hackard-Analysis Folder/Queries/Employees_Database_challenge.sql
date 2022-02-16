48a6138b0253350e33edeb8577e93561503d591d
Pa Lor <palor@Pas-MacBook-Air-3.local>
2022-02-16 00:45:28 -0600

    adding retirement & eligible employees

---
+++ Pewlett-Hackard-Analysis Folder/Queries/Employees_Database_challenge.sql
@@ -0,0 +1,43 @@
+-- Retiring employees by title
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO distinct_retirement_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+--Count of retirees by title
+SELECT count (emp_no), title
+INTO retiring
+FROM distinct_retirement_titles
+GROUP BY title
+ORDER BY count (emp_no) DESC;
+
+--Mentorship eligibility
+SELECT DISTINCT ON (emp_no) e.emp_no, 
+e.first_name,
+e.last_name, 
+e.birth_date, 
+d.from_date, 
+d.to_date, 
+t.title
+INTO eligible_mentorship
+FROM employees as e
+INNER JOIN dept_emp as d
+ON (e.emp_no = d.emp_no)
+INNER JOIN titles as t
+ON (d.emp_no = t.emp_no)
+WHERE (d.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
